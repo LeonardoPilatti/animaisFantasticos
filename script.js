57cfeae590784deaa68e20c28e91bd76fbb08808
Your Name <Leonardopilatti14@gmail.com>
2021-01-17 13:55:12 -0300

    Animacao ao scroll

--- script.js
+++ script.js
@@ -82,3 +82,31 @@ function initScrollSuave(){
     });
 }
 initScrollSuave();
+
+/// aqui para baixo é para fazer as animações de scroll:
+function initAnimacaoScroll() {
+
+    const sections = document.querySelectorAll('.js-scroll');
+
+    if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;  /// esse é para pegar 60% da tela do usuário
+
+    function animaScroll() {
+        sections.forEach((sectionItem) => {
+            const sectionTop = sectionItem.getBoundingClientRect().top;
+            const isSectionVisible = (sectionTop - windowMetade) < 0;
+            if(isSectionVisible) {
+                sectionItem.classList.add('ativo');
+            } else {
+                sectionItem.classList.remove('ativo');
+            }
+        })
+    // console.log('sim');  // para testar se está funcionando
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll' , animaScroll);  /// tenho que falar com o window, pois o scroll é do window
+    }
+}
+initAnimacaoScroll();
