d63a116e0ebaed02a06b5e95ade0c574e7c82075
Your Name <Leonardopilatti14@gmail.com>
2021-01-26 20:51:56 -0300

    corrigindo erro menu mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,22 +1,20 @@
-import outsideClick from './outsideClick.js' 
+import outsideClick from './outsideClick.js';
 
 export default function initMenuMobile() {
-    const menuButton = document.querySelector('[data-menu="button"]');
-    const menuList = document.querySelector('[data-menu="list"]');
-    const eventos = ['click', 'touchstart'];
-    
-    if(menuButton) {
-        function openMenu(event) {
-            menuList.classList.add('ativo');
-            menuButton.classList.add('ativo');
-            outsideClick(menuList, eventos, () => {
-                menuList.classList.toggle('ativo');
-                menuButton.classList.toggle('ativo');  
-            })
-        }
-        
-        eventos.forEach((evento) => {
-            menuButton.addEventListener(evento, openMenu);
-        })
-    }
+  const menuButton = document.querySelector('[data-menu="button"]');
+  const menuList = document.querySelector('[data-menu="list"]');
+  const eventos = ['click', 'touchstart'];
+  
+  if(menuButton) {
+  function openMenu(event) {
+    menuList.classList.add('ativo');
+    menuButton.classList.add('ativo');
+    outsideClick(menuList, eventos, () => {
+    console.log('teste')
+      menuList.classList.remove('ativo');
+      menuButton.classList.remove('ativo');
+    })
+  }
+  eventos.forEach(evento => menuButton.addEventListener(evento, openMenu));
+  }
 }
--- js/modules/outsideClick.js
+++ js/modules/outsideClick.js
@@ -1,27 +1,23 @@
-/// esse outsideClick é para quando clicar fora do menu, ele fechar o menu ou quando clicar fora do dropdownMenu, ele sair
 export default function outsideClick(element, events, callback) {
-    const html = document.documentElement;    /// aqui estou selecionando o html, para quando clicar fora do menu do sobre, ele feche
-    const outside = 'data-outside';
+  const html = document.documentElement;
+  const outside = 'data-outside';
 
-    if(!element.hasAttribute(outside)) {
-      events.forEach((userEvent) => {
-        setTimeout(() => {html.addEventListener(userEvent, handleOutsideClick)});  /// esse setTimeOut faz o addeventlistener esperar na fila para carregar 
-      });
-
-      html.addEventListener('click', handleOutsideClick);  /// aqui estou adicionando o evento de clicar fora e ativando a funcao
-      element.setAttribute(outside, '');
-    }
-    function handleOutsideClick(event) {    /// aqui estou criando a funcao que será ativada no clicar, criei dentro para ela somente  ser criada se for ativiada a funcao
-      if(!element.contains(event.target)) {  /// se for falso, por causa do '!', ele irá executar a função de callback, se for verdadeiro, no caso clicar dentro do dropdown-menu, ele não irá executar a função, ou seja, irá ficar aberto
-        element.removeAttribute(outside);
-
-        events.forEach((userEvent) => {
-          html.removeEventListener(userEvent, handleOutsideClick);  /// para remover o callback do html
-        }) 
-        callback();
-      }
+  if(!element.hasAttribute(outside)) {
+    events.forEach(userEvent => {
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+    });
+    element.setAttribute(outside, '');
+  }
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach(userEvent => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      })
+      callback();
     }
   }
+}
 
 
 
